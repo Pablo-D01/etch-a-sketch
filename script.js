17db170c1f42312ee8c29c57ec7b1692172d4334
Paweł D <you@example.com>
2023-03-08 22:23:06 +0100

    dodałem możliwość rysowania

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 // Get the container element using its id
-var container = document.getElementsByClassName("container");
+let container = document.getElementsByClassName("container");
+
+function paint(){
+  console.log('Najechano na element');
+  const targetElement = event.target;
+  targetElement.style.backgroundColor = 'black';}
 
 for (let i = 0; i < 256; i++) {
     var newDiv = document.createElement("div");
     newDiv.setAttribute("class", "grid-div");
-    container[0].appendChild(newDiv);
 
+    
+    container[0].appendChild(newDiv);
   }
   
+  // let gridDiv = document.getElementsByClassName("grid-div");
+  // gridDiv.addEventListener('mousedown', (event) => {      
+  //   paint();
+  // });
+
+let isPainting = false;
+
+const divs = document.querySelectorAll('div.grid-div');
+
+divs.forEach(div => {
+  div.addEventListener('mousedown', (event) => {      
+    paint();
+  });
+});

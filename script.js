843faa825bf1f2a36de348f873d0703061237cb9
Paweł D <you@example.com>
2023-03-08 21:44:40 +0100

    dodałem siatkę

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Get the container element using its id
 var container = document.getElementsByClassName("container");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     var newDiv = document.createElement("div");
-    newDiv.textContent = "X";
+    newDiv.setAttribute("class", "grid-div");
     container[0].appendChild(newDiv);
 
   }
